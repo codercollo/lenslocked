feb0b021234b7fe2029651e414c4fe10db435ad3
codercollo <itscollinsmaina@gmail.com>
2026-02-16 10:33:40 +0300

    Add chi - router

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</><p>To get in touch, email me at <a href=\"mailto:itscollinsmaina@gmail.com\">itscollinsmaina@gmail.com</a></p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>Contact Page</h1>
+			<ul>
+				<li><b>Is there a free version?</b> Yes! We offer a free trial for 30 days on any 
+				paid plans.
+				</li>
+								<li><b>What are your support hours?</b> Yes! We have support staff answering emails 24/7, through reponse times may be a bit slower on weekends. 
+				</li>
+								<li><b>How do I contact support?</b> Email us - support@lenslocked.com.
+				</li>
+			</ul>
+	`)
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+func main() {
+	var router Router
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", router)
+
+}
